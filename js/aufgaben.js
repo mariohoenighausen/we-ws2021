c82df9ac812cd929e105b648fb1910aa762ee1a3
mariohoenighausen <m.hoenighausen@gmail.com>
2021-10-14 10:03:39 +0200

    Added openAufgabe function for tabination and  scroll funtion for a sticky tab menu

---
+++ js/aufgaben.js
@@ -0,0 +1,31 @@
+function openAufgabe(aufgabe) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(aufgabe).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+// Quelle https://www.w3schools.com/howto/howto_js_tabs.asp
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("tabBar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
